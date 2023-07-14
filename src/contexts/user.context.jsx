825cfd231aa0006b4e587ae9968e792c2cfc23d2
naufaladdhiya <naufal.addhiya@gmail.com>
2023-07-14 22:48:05 +0700

    feat: implement reducer to userContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -9,8 +9,34 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTIONS_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+export const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTIONS_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unknown action type: ${type}`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
